36920a62dafe6a532697f432f22d01a95d02d89c
Tony Childs <tony.childs@gmail.com>
2021-04-04 09:03:24 -0500

    Generate and integrate search component stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ImageSearchComponent } from './image-search/image-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSearchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/image-search/image-search.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'image-search',
+  templateUrl: './image-search.component.html',
+  styleUrls: ['./image-search.component.scss'],
+})
+export class ImageSearchComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
