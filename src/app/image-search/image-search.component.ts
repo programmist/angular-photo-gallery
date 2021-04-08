b403f17f9566d7dadd7ab84a948c6c943453b26e
Tony Childs <tony.childs@gmail.com>
2021-04-08 17:11:16 -0500

    clean up IntersectionObserver

--- src/app/image-search/image-search.component.ts
+++ src/app/image-search/image-search.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Image } from '../types/Image';
 import { MediaService } from '../media.service';
@@ -13,7 +19,7 @@ import { RESPONSE } from './mock-search-response';
   templateUrl: './image-search.component.html',
   styleUrls: ['./image-search.component.scss'],
 })
-export class ImageSearchComponent implements OnInit, AfterViewInit {
+export class ImageSearchComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('loading') loadingIndicator: ElementRef<HTMLElement>;
   images: Image[] = RESPONSE.photos; //[]; TODO: Remove after testing
   private observer: IntersectionObserver;
@@ -26,20 +32,23 @@ export class ImageSearchComponent implements OnInit, AfterViewInit {
     private host: ElementRef<HTMLElement>
   ) {}
 
+  ngOnInit(): void {
+    this.imageStream.subscribe((images: Image[]) => {
+      this.images = [...this.images, ...images];
+    });
+  }
+
   ngAfterViewInit(): void {
     this.observer = new IntersectionObserver(([entry]) => {
       if (entry.isIntersecting && this.images.length > 0) {
         this.nextPage();
       }
     }, {});
-
     this.observer.observe(this.loadingIndicator.nativeElement);
   }
 
-  ngOnInit(): void {
-    this.imageStream.subscribe((images: Image[]) => {
-      this.images = [...this.images, ...images];
-    });
+  ngOnDestroy(): void {
+    this.observer.disconnect();
   }
 
   // TODO: DRY up repetitive code
